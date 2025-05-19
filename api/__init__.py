78515ef3491772f02338d417a6e097b465b36fdc
wallisonc2k <wallisonlino137@gmail.com>
2025-05-19 17:50:46 -0300

    Primeira versão de configuraçôes

--- api/__init__.py
+++ api/__init__.py
@@ -14,8 +14,8 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 jwt = JWTManager(app)
 
-from api.models import cabine_model, pallet_model
-from api.controller import cabine_controller, pallet_controller
+from api.models import pallet_model
+from api.controller import pallet_controller
 
 app.register_blueprint(pallet_controller.pallet_bp, url_prefix='/api')
 
@@ -27,16 +27,8 @@ def home():
 def configuracao():
     return render_template('configuracao.html')
 
-@app.route("/cabines")
-def cabines():
-    variedades = ["Arra 15", "White Seedless", "Sugar Crisp", "IFG Eleven", "Autumn Crisp","White Seedless 02", "Sweet Celebration", "IFG Three"]
-    return render_template("cabines.html", variedades=variedades)
-
 @app.route("/pallets")
 def pallets():
     ip = get_local_ip()
+    variedades = ["Arra 15", "White Seedless", "Sugar Crisp", "IFG Eleven", "Autumn Crisp","White Seedless 02", "Sweet Celebration", "IFG Three"]
     return render_template("pallets.html", api_url_ip=ip)
-
-@app.route("/cabines-admin")
-def cabine_admin():
-    return render_template("cabines_admin.html")
